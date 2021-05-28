6c9b60f8bf400d63f4a7e320be33a0572105a03d
kamilcodes <kc.chrzanowski@gmail.com>
2021-05-28 11:37:36 +0200

    added active status for accordion-panel and adjusted global media queries

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const openPanel = function () {};
 for (let i = 0; i < accordionPanel.length; i++) {
   accordionPanel[i].addEventListener("click", function () {
     accordionPanel[i].nextElementSibling.classList.toggle("hidden");
+    accordionPanel[i].classList.toggle("accordion-panel__active");
     accordionContent[i].style.animation = "animateIn .2s ease-out";
   });
 }
